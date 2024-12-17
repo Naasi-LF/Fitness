d9f7779af05bdce75d1bb8aba8acd5824988adc6
Naasi-LF <2737492311@qq.com>
2024-12-18 07:03:17 +0800

    master

--- obj/Debug/net8.0-windows/Fitness.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Fitness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1380d294c33c7cd44135fff0b710878075ac0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4cc9eeffa9e98f5c9b73eda1eb1def51bef2cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
